1f86cf341514db23d45af1778504782abc7f90a2
Juan Castro <juancastro@firstfactory.com>
2021-05-08 21:19:26 -0600

    Finished the menu bar and added responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import GlobalStyle from './globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
-      <h2>Styled Components</h2>
     </Router>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
+import { Button } from '../../globalStyles'
 import * as Styled from './Navbar.styled';
 
 const Navbar = () => {
+    const [ click, setClick ] = useState(false);
+    const [ button, setButton ] = useState(true);
+
+    const handleClick = () => setClick(!click);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton);
+
     return (
         <Styled.Navbar>
             <Styled.NavbarContainer>
-                <Styled.NavLogo>
+                <Styled.NavLogo to='/'>
                     <Styled.NavIcon />
                     ULTRA
                 </Styled.NavLogo>
+                <Styled.MobileIcon onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </Styled.MobileIcon>
+                <Styled.NavMenu onClick={handleClick} click={click}>
+                    <Styled.NavItem>
+                        <Styled.NavLinks to='/'>
+                            Home
+                        </Styled.NavLinks>
+                    </Styled.NavItem>
+                    <Styled.NavItem>
+                        <Styled.NavLinks to='/services'>
+                            Services
+                        </Styled.NavLinks>
+                    </Styled.NavItem>
+                    <Styled.NavItem>
+                        <Styled.NavLinks to='/products'>
+                            Products
+                        </Styled.NavLinks>
+                    </Styled.NavItem>
+                    <Styled.NavItemBtn>
+                        {button ? (
+                            <Styled.NavBtnLink to='/sign-up'>
+                                <Button primary>SIGN UP</Button>
+                            </Styled.NavBtnLink>
+                        ) : (
+                            <Styled.NavBtnLink to='/sign-up'>
+                                <Button fontBig primary>
+                                    SIGN IN
+                                </Button>
+                            </Styled.NavBtnLink>
+                        )}
+                    </Styled.NavItemBtn>
+                </Styled.NavMenu>
             </Styled.NavbarContainer>
         </Styled.Navbar>
     )
